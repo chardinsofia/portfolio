1ad87efb6b1dadc854c913aebbd12368ab722572
chardinsofia <chardinsofia@gmail.com>
2023-12-31 00:23:58 -0300

    bunch of implementations, added fonts, sections, improved navbar and responsiveness

--- js/index.js
+++ js/index.js
@@ -1,13 +1,49 @@
 $(function() {
-        $(".toggle").on("click", function() {
-                if ($(".item").hasClass("active")) {
-                    $(".item").removeClass("active");
-                    $(this).find("a").html("<i class='fas fa-bars'></i>");
-                }
+    $(".toggle, .item").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+            $(".toggle").find("a").html("<i class='fas fa-bars'></i>");
+        }
 
-                else {
-                    $(".item").addClass("active");
-                    $(this).find("a").html("<i class='fas fa-times'></i>");
-                }
-            });
+        else {
+            $(".item").addClass("active");
+            $(".toggle").find("a").html("<i class='fas fa-times'></i>");
+        }
     });
+});
+
+
+var skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Illustrator',
+  'Premiere Pro',
+  'SASS',
+  'Design',
+  'Creativity',
+  'English',
+  'Spanish',
+  'Writing',
+  'Photography',
+  'Learning'
+];
+
+var skillCount = 0;
+
+function changeSkills() {
+  renderSkills(skillCount);
+  if (skillCount < skills.length - 1) {
+    skillCount++;
+  } else {
+    skillCount = 0;
+  }
+  setTimeout(function() {
+      changeSkills();
+  }, 1000);
+}
+
+function renderSkills(n) {
+  document.getElementById("skill").setAttribute("data-content", skills[n]);
+}
+changeSkills();
